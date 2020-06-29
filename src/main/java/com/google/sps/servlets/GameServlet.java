f0ba35b6e355fb4041e1437f41c71b84beb10ca7
Salil <salnad@gmail.co>
2020-06-29 17:34:34 -0400

    Rearrange javascript to make code easier to understand. Remove additional lines of code and comments from html, java, javascript.

--- src/main/java/com/google/sps/servlets/GameServlet.java
+++ src/main/java/com/google/sps/servlets/GameServlet.java
@@ -1,17 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package com.google.sps.servlets;
 
 import com.google.sps.data.SubtractionGame;
@@ -31,12 +17,9 @@ import com.wrapper.spotify.requests.data.playlists.GetPlaylistsItemsRequest;
 import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 import org.apache.hc.core5.http.ParseException;
-import com.google.gson.Gson;
+import com.google.gson.Gson; 
 import java.util.ArrayList;
 
- 
-
-/** Servlet that encapsulates the subtraction game. */
 @WebServlet("/game")
 public final class GameServlet extends HttpServlet {
 
@@ -50,14 +33,6 @@ public final class GameServlet extends HttpServlet {
     response.setContentType("application/json;");
     response.getWriter().println(jsonData);
 
-  }
-
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    System.out.println(request.getParameter("trackList"));
-    // String playlistLink = request.getParameter("playlist-link");
-    // System.out.println(playlistLink);
-    // response.sendRedirect("/index.html");
   }
 
   private String getAuthToken() {
@@ -74,26 +49,5 @@ public final class GameServlet extends HttpServlet {
     }
     return "";
   }
-
-  // private void getSpotifyPlaylist(){
-    
-  //   String playlistId = "3AGOiaoRXMSjswCLtuNqv5";
-
-    
-
-
-
-  //   // GetPlaylistRequest getPlaylistRequest = spotifyApi.getPlaylist(playlistId).build();
-  //   GetPlaylistsItemsRequest getPlaylistsItemsRequest = spotifyApi.getPlaylistsItems(playlistId).build();
-  //   try {
-  //     PlaylistTrack tracks[] = getPlaylistsItemsRequest.execute().getItems();
-  //     for (PlaylistTrack track : tracks) {
-  //       System.out.println(track.getTrack().toString());
-  //     }
-
-  //   } catch (IOException | SpotifyWebApiException | ParseException e) {
-  //     System.out.println("Error: " + e.getMessage());
-  //   }  
-  // }
 
 }
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,31 +1,36 @@
+async function playTrackFromPlaylist() {
+  let previewURL = await getRandomPreviewURL();
+  document.getElementById("song-url").src = previewURL;
+  audio.load();
+  audio.play();
+}
+
+async function getRandomPreviewURL() {
+  let playlist = await getPlaylist();
+  let trackList = playlist.tracks.items;
+  var randomTrack = trackList[Math.floor(Math.random() * trackList.length)];
+  var previewURL = randomTrack.track.preview_url;
+  return previewURL;
+}
+
+async function getPlaylist() {
+  let authToken = await getAuthToken();
+  let spotifyApi = new SpotifyWebApi();
+  spotifyApi.setAccessToken(authToken);
+  let playlistID = getIdFromURL(document.getElementById("playlist-id").value);
+  let playlist = await spotifyApi.getPlaylist(playlistID);
+  return playlist;
+}
+
+async function getAuthToken() {
+  let authTokenRequest = await fetch('/game', {method: 'GET'});
+  let authTokenResponse = await authTokenRequest.json();
+  let authToken = authTokenResponse[0];
+  return authToken;
+}
 
 function getIdFromURL(url) {
   var start = url.lastIndexOf('/') + 1;
   var end = url.indexOf('?', start);
   return url.slice(start, end);
 }
-
-async function getAuthToken() {
-  let authTokenRequest = await fetch('/game', {method: 'GET'});
-  let authToken = await authTokenRequest.json();
-  let myToken = authToken[0];
-  let spotifyApi = new SpotifyWebApi();
-  spotifyApi.setAccessToken(myToken);
-  let playlistID = getIdFromURL(document.getElementById("playlist-id").value);
-  console.log(playlistID);
-  let playlist = await spotifyApi.getPlaylist(playlistID);
-  let trackList = playlist.tracks.items;
-  var previewURL = trackList[Math.floor(Math.random() * trackList.length)].track.preview_url;
-  console.log(previewURL);
-  document.getElementById("song-url").src = previewURL;
-  audio.load();
-  audio.play();
-  console.log(trackList);
-
-}
-
-let url = "https://open.spotify.com/playlist/0vvXsWCC9xrXsKd4FyS8kM?si=0pUHoH8QSt-go4_x4SvGVA";
-
-
-getAuthToken();
-
