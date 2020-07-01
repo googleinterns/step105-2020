ad6ac4862bcf70c9e4854cc8d0061742393e919a
dee <hdee@google.com>
2020-07-01 08:13:25 -0400

    import playlistURL to playlist.js

--- src/main/webapp/playlist.js
+++ src/main/webapp/playlist.js
@@ -1,8 +1,9 @@
 // imports
 const authToken = require('/.authToken');
+const playlistURL = require('./playlistURL')
 
 async function playTrackFromPlaylist() {
-  let previewURL = await getRandomPreviewURL();
+  let previewURL = await playlistURL.getRandomPreviewURL();
   document.getElementById("song-url").src = previewURL;
   audio.load();
   audio.play();
@@ -13,7 +14,7 @@ async function getPlaylist() {
   let authToken = await authToken.getAuthToken();
   let spotifyApi = new SpotifyWebApi();
   spotifyApi.setAccessToken(authToken);
-  let playlistID = getIdFromURL(document.getElementById("playlist-id").value);
+  let playlistID = playlistURL.getIdFromURL(document.getElementById("playlist-id").value);
   let playlist = await spotifyApi.getPlaylist(playlistID);
   return playlist;
 }
