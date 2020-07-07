f6f67c13324092fd01b1730187301fffa7c0efa9
dee <hdee@google.com>
2020-07-07 07:06:30 -0400

    add dopost method

--- src/main/java/com/google/sps/servlets/GameServlet.java
+++ src/main/java/com/google/sps/servlets/GameServlet.java
@@ -1,7 +1,5 @@
 package com.google.sps.servlets;
 
-import com.google.sps.data.SubtractionGame;
-import com.google.sps.ApiExample;
 import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
@@ -9,17 +7,78 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.PlaylistListResponse;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collection;
+
 @WebServlet("/game")
 public final class GameServlet extends HttpServlet {
-  private ApiExample example = new ApiExample();
+
+    private static final String DEVELOPER_KEY = "AIzaSyBZw4Z25Lect7ux9z960RCM7YORcYo6slc";
+
+    private static final String APPLICATION_NAME = "API code samples";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
-    String json = new Gson().toJson(example);
+    String json = new Gson().toJson(main("PLOU2XLYxmsIL32BnYVuSqwgS0ClB_csKd"));
     response.getWriter().println(json);
-     System.out.println("HELLLOOOOOOO^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^THER");
+    System.out.println("HELLLOOOOOOO^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^THER");
   }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Get the input from the form.
+    String link = getParameter(request, "playlist-link", "");
+
+  }
+
+
+    /**
+     * Build and return an authorized API client service.
+     *
+     * @return an authorized API client service
+     * @throws GeneralSecurityException, IOException
+     */ 
+  public static YouTube getService() throws GeneralSecurityException, IOException {
+        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
+            .setApplicationName(APPLICATION_NAME)
+            .build();
+    }
+
+  /**
+     * Call function to create API service object. Define and
+     * execute API request. Print API response.
+     *
+     * @throws GeneralSecurityException, IOException, GoogleJsonResponseException
+     */
+    public static PlaylistListResponse main(String playlistID)
+        throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+        YouTube youtubeService = getService();
+        // Define and execute the API request
+        YouTube.Playlists.List request = youtubeService.playlists()
+            .list("snippet,contentDetails,player");
+        PlaylistListResponse response = request.setKey(DEVELOPER_KEY)
+            .setId(playlistID)
+            .setMaxResults(25L)
+            .setMine(false)
+            .execute();
+        System.out.println(response);
+        return response;
+    }
 
 
 }
--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,37 +1,35 @@
-async function playTrackFromPlaylist() {
-  let previewURL = await getRandomPreviewURL();
-  let audio = document.getElementById("audio");
-  document.getElementById("song-url").src = previewURL;
-  audio.load();
-  audio.play();
-}
+// async function playTrackFromPlaylist() {
+//   let previewURL = await getRandomPreviewURL();
+//   let audio = document.getElementById("audio");
+//   document.getElementById("song-url").src = previewURL;
+//   audio.load();
+//   audio.play();
+// }
 
-async function getRandomPreviewURL() {
-  let playlist = await getPlaylist();
-  let trackList = playlist.tracks.items;
-  var randomTrack = trackList[Math.floor(Math.random() * trackList.length)];
-  var previewURL = randomTrack.track.preview_url;
-  return previewURL;
-}
+// async function getRandomPreviewURL() {
+//   let playlist = await getPlaylist();
+//   let trackList = playlist.tracks.items;
+//   var randomTrack = trackList[Math.floor(Math.random() * trackList.length)];
+//   var previewURL = randomTrack.track.preview_url;
+//   return previewURL;
+// }
 
-async function getPlaylist() {
-  let authToken = await getAuthToken();
-  let spotifyApi = new SpotifyWebApi();
-  spotifyApi.setAccessToken(authToken);
-  let playlistID = getIdFromURL(document.getElementById("playlist-id").value);
-  let playlist = await spotifyApi.getPlaylist(playlistID);
-  return playlist;
-}
+// function getPlaylistID() {
+//   let playlistID = getIdFromURL(document.getElementById("playlist-id").value);
+//   return playlistID;
+// }
 
-async function getAuthToken() {
-  let authTokenRequest = await fetch('/game', {method: 'GET'});
-  let authTokenResponse = await authTokenRequest.json();
-  let authToken = authTokenResponse[0];
-  return authToken;
-}
 
-function getIdFromURL(url) {
-  var start = url.lastIndexOf('/') + 1;
-  var end = url.indexOf('?', start);
-  return url.slice(start, end);
+
+// function getIdFromURL(url) {
+//   var start = url.lastIndexOf('/') + 1;
+//   var end = url.indexOf('?', start);
+//   return url.slice(start, end);
+// }
+
+function embedPlaylist(){
+fetch ('/game').then(response => response.json()).then((playlist) => {
+  const ytPlayer = document.getElementById("ytplayer");
+  ytPlayer.innerHTMl = playlist.player.embedHtml;
+});
 }
