544088ce76aecb7c43041e2aab5142cded22926e
dee <hdee@google.com>
2020-07-07 11:03:23 -0400

    changed youtube playlist request to arrat

--- src/main/java/com/google/sps/servlets/GameServlet.java
+++ src/main/java/com/google/sps/servlets/GameServlet.java
@@ -7,6 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -14,22 +19,28 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 
 import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.PlaylistListResponse;
+import com.google.api.services.youtube.model.PlaylistItemListResponse;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 
 @WebServlet("/game")
 public final class GameServlet extends HttpServlet {
 
     private static final String DEVELOPER_KEY = "AIzaSyBZw4Z25Lect7ux9z960RCM7YORcYo6slc";
+    private static final String CLIENT_SECRETS= "client_secret.json";
+    private static final Collection<String> SCOPES =
+        Arrays.asList("https://www.googleapis.com/auth/youtube.readonly");
 
     private static final String APPLICATION_NAME = "API code samples";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-    private PlaylistListResponse information = new PlaylistListResponse();
+    private PlaylistItemListResponse information = new PlaylistItemListResponse();
 
 
   @Override
@@ -37,7 +48,6 @@ public final class GameServlet extends HttpServlet {
     response.setContentType("application/json");
     String json = new Gson().toJson(information);
     response.getWriter().println(json);
-    System.out.println("HELLLOOOOOOO^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^THERE");
   }
 
   @Override
@@ -45,7 +55,12 @@ public final class GameServlet extends HttpServlet {
     // Get the input from the form.
     String link = getParameter(request, "playlist-link", "");
     String playlistID = getIdFromURL(link);
-    information = getPlaylistInfo(playlistID);
+    try{
+       information = getPlaylistInfo(playlistID);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    
   }
 
   public String getIdFromURL(String url){
@@ -56,7 +71,25 @@ public final class GameServlet extends HttpServlet {
    return "";
   }
 
-
+    /**
+     * Create an authorized Credential object.
+     *
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    public static Credential authorize(final NetHttpTransport httpTransport) throws IOException {
+        // Load client secrets.
+        InputStream in = GameServlet.class.getResourceAsStream(CLIENT_SECRETS);
+        GoogleClientSecrets clientSecrets =
+          GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+            new GoogleAuthorizationCodeFlow.Builder(httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
+            .build();
+        Credential credential =
+            new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
+        return credential;
+    }
 
     /**
      * Build and return an authorized API client service.
@@ -64,29 +97,29 @@ public final class GameServlet extends HttpServlet {
      * @return an authorized API client service
      * @throws GeneralSecurityException, IOException
      */ 
-  public static YouTube getService() throws GeneralSecurityException, IOException {
+    public static YouTube getService() throws GeneralSecurityException, IOException {
         final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
+        Credential credential = authorize(httpTransport);
+        return new YouTube.Builder(httpTransport, JSON_FACTORY, credential)
             .setApplicationName(APPLICATION_NAME)
             .build();
     }
 
-  /**
+
+       /**
      * Call function to create API service object. Define and
      * execute API request. Print API response.
      *
      * @throws GeneralSecurityException, IOException, GoogleJsonResponseException
      */
-    public static PlaylistListResponse getPlaylistInfo(String playlistID)
+    public static PlaylistItemListResponse getPlaylistInfo(String playlistID)
         throws GeneralSecurityException, IOException, GoogleJsonResponseException {
         YouTube youtubeService = getService();
         // Define and execute the API request
-        YouTube.Playlists.List request = youtubeService.playlists()
-            .list("snippet,contentDetails,player");
-        PlaylistListResponse response = request.setKey(DEVELOPER_KEY)
-            .setId(playlistID)
-            .setMaxResults(25L)
-            .setMine(false)
+        YouTube.PlaylistItems.List request = youtubeService.playlistItems()
+            .list(Arrays.asList("snippet", "contentDetails"));
+        PlaylistItemListResponse response = request.setMaxResults(25L)
+            .setPlaylistId(playlistID)
             .execute();
         System.out.println(response);
         return response;
