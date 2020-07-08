a55f2d10eaf31fc3694bc2a3103e3edf45399356
Isis Black <isismblack@gmail.com>
2020-07-08 10:00:07 -0400

    Create new user, store userID in cookie

---
+++ src/main/webapp/room.js
@@ -0,0 +1,22 @@
+let players = [];
+
+const createUser = (ev)=>{
+    ev.preventDefault();  //to stop the form submitting
+    let user = {
+        username: document.getElementById('new-player-id').value,
+        get userID() {
+            // Base 36 (numbers + letters), and grab the first 9 characters after decimal.
+            return '_' + Math.random().toString(36).substr(2, 9);
+        }
+    }
+    players.push(user);
+
+    console.log('added', {players});
+
+    //saving to cookies
+    //document.cookie = 'username=' + user.username + '; expires=' + new Date(2025, 0, 1).toUTCString();
+    document.cookie = 'userID=' + user.userID + '; expires=' + new Date(2025, 0, 1).toUTCString();
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+    document.getElementById('new-player-btn').addEventListener('click', createUser);
+});
