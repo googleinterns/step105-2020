9031a9391a747fa34f4180b9533f9bb498558b55
dee <hdee@google.com>
2020-07-09 11:49:34 -0400

    commented out one line

--- src/main/java/com/google/sps/servlets/RoomServlet.java
+++ src/main/java/com/google/sps/servlets/RoomServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.hc.core5.http.ParseException;
+// import org.apache.hc.core5.http.ParseException;
 import com.google.gson.Gson; 
 import java.util.ArrayList;
 
