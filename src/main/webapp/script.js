5d0163f0477ee1c91368fbc2f891f577d3170439
dee <hdee@google.com>
2020-07-10 10:01:12 -0400

    add game page

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,35 +1,6 @@
-// async function playTrackFromPlaylist() {
-//   let previewURL = await getRandomPreviewURL();
-//   let audio = document.getElementById("audio");
-//   document.getElementById("song-url").src = previewURL;
-//   audio.load();
-//   audio.play();
-// }
-
-// async function getRandomPreviewURL() {
-//   let playlist = await getPlaylist();
-//   let trackList = playlist.tracks.items;
-//   var randomTrack = trackList[Math.floor(Math.random() * trackList.length)];
-//   var previewURL = randomTrack.track.preview_url;
-//   return previewURL;
-// }
-
-// function getPlaylistID() {
-//   let playlistID = getIdFromURL(document.getElementById("playlist-id").value);
-//   return playlistID;
-// }
-
-
-
-// function getIdFromURL(url) {
-//   var start = url.lastIndexOf('/') + 1;
-//   var end = url.indexOf('?', start);
-//   return url.slice(start, end);
-// }
 
 function embedPlaylist(){
-fetch ('/game').then(response => response.json()).then((playlist) => {
-  const ytPlayer = document.getElementById("ytplayer");
-  ytPlayer.innerHTMl = playlist.items;
+fetch ('/game').then(response => response.json()).then(() => {
+
 });
 }
