2455d82f7c057238e99706a0ef601ea25398504b
Isis Black <isismblack@gmail.com>
2020-07-09 22:34:03 -0400

    PR fixes

--- src/main/webapp/room.js
+++ src/main/webapp/room.js
@@ -1,18 +1,12 @@
-let players = [];
-
-const createUser = (ev)=>{
-    ev.preventDefault();  //to stop the form submitting
+const createUser = (ev) => {
+    ev.preventDefault();  // To stop the form submitting.
     let user = {
         username: document.getElementById('new-player-id').value,
-        get userID() {
-            // Base 36 (numbers + letters), and grab the first 9 characters after decimal.
-            return '_' + Math.random().toString(36).substr(2, 9);
-        }
+        // Base 36 (numbers + letters), and grab the first 9 characters after decimal.
+        userId: '_' + Math.random().toString(36).substr(2, 9)
     }
-    players.push(user);
-
-    //saving to cookies
-    document.cookie = 'userID=' + user.userID + '; expires=' + new Date(2025, 0, 1).toUTCString();
+    // Save userId to cookie.
+    document.cookie = 'userId=' + user.userId + '; expires=' + new Date(2025, 0, 1).toUTCString();
 }
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('new-player-btn').addEventListener('click', createUser);
