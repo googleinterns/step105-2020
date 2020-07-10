fb4162369e168d686074425d38e6c5a1f220179e
dee <hdee@google.com>
2020-07-10 11:31:56 -0400

    remove youtube div from index

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -3,5 +3,4 @@ function embedPlaylist(){
 fetch ('/game').then(response => response.json()).then((videoID) => {
   document.getElementById("player").src = "http://www.youtube.com/embed/" + videoID;
 });
-
 }
