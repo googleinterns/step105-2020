f54afc3ae85161a8271b1fabc2b3628557b413d1
Salil <salnad02@gmail.com>
2020-07-13 14:30:41 -0400

    Testing Jest with webhook

--- src/main/webapp/script.test.js
+++ src/main/webapp/script.test.js
@@ -1,17 +1,19 @@
-const script = require('./script');
+const script = require("./script");
 
-
-test('extract playlist id from address bar url', () => {
+test("extract playlist id from address bar url", () => {
   expect(
-    script.getIdFromURL("https://open.spotify.com/playlist/37i9dQZF1DX28ZIZjK8SGt")
-  ).toBe("37i9dQZF1DX28ZIZjK8SGt")
-})
+    script.getIdFromURL(
+      "https://open.spotify.com/playlist/37i9dQZF1DX28ZIZjK8SGt"
+    )
+  ).toBe("37i9dQZF1DX28ZIZjK8SGtLOL");
+});
 
-test('extract playlist id from url that spotify gives', () => {
+test("extract playlist id from url that spotify gives", () => {
   expect(
-    script.getIdFromURL("https://open.spotify.com/playlist/37i9dQZF1DX28ZIZjK8SGt?si=_4J20xQJQxyB1ld1ZxOJQg")
-  ).toBe("37i9dQZF1DX28ZIZjK8SGt")
-})
+    script.getIdFromURL(
+      "https://open.spotify.com/playlist/37i9dQZF1DX28ZIZjK8SGt?si=_4J20xQJQxyB1ld1ZxOJQg"
+    )
+  ).toBe("37i9dQZF1DX28ZIZjK8SGt");
+});
 
 // Add testing to check that the function does not accept non=Spotify links
-
