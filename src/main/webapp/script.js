c3ed7529ddd35bd0f5bc3c1dbff7dfbf534cabc3
dee <hdee@google.com>
2020-07-15 04:04:50 -0400

    combine chat and script js files

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,8 +1,1 @@
 
-function embedPlaylist(){
-fetch ('/game').then(response => response.json()).then((videoID) => {
-  document.getElementById("player").src = "http://www.youtube.com/embed/" + videoID;
-});
-}
-
-// Add testing exports here
