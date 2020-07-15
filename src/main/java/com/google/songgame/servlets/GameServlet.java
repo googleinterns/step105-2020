43b60a7354b8f262fa853db194e5b8f5d4ff04fd
Salil <salnad02@gmail.com>
2020-07-15 17:47:32 -0400

    Semicolon fix

--- src/main/java/com/google/songgame/servlets/GameServlet.java
+++ src/main/java/com/google/songgame/servlets/GameServlet.java
@@ -23,7 +23,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import com.google.api.client.json.gson.GsonFactory;
-setvideo
+
 @WebServlet("/game")
 public final class GameServlet extends HttpServlet {
 
@@ -84,7 +84,7 @@ public final class GameServlet extends HttpServlet {
     }
     // Parse Playlist item Json string to retrieve video IDs
     String playlistItemJson = new Gson().toJson(playlistItem);
-    ArrayList<String> playlistVideos = parseVideoIdsFromPlaylistItem(playlistItemJson)
+    ArrayList<String> playlistVideos = parseVideoIdsFromPlaylistItem(playlistItemJson);
     videoId = getRandomVideo(playlistVideos);
   }
 
