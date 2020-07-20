07cdd389ae5241c80c901f1471e080f4edeb3c58
Isis Black <isismblack@gmail.com>
2020-07-20 17:34:23 -0400

    Fix datastore varible

--- src/main/java/com/google/songgame/servlets/RoomServlet.java
+++ src/main/java/com/google/songgame/servlets/RoomServlet.java
@@ -29,11 +29,12 @@ public final class RoomServlet extends HttpServlet {
 
   private final static Type MESSAGE_TYPE = new TypeToken<Map<String, String>>(){}.getType();
   private Gson gson;
+  DatastoreService datastore;
 
   @Override
   public void init() {
     gson = new Gson();
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore = DatastoreServiceFactory.getDatastoreService();
   }
 
   @Override
