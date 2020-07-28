9d04808fba5e1240f8334cf8a2cc81251808cc55
Salil <salnad02@gmail.com>
2020-07-28 16:48:35 -0400

    Reformatted tests into seperate tests

--- src/test/java/com/google/songgame/TitleFormatterTest.java
+++ src/test/java/com/google/songgame/TitleFormatterTest.java
@@ -12,16 +12,23 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class TitleFormatterTest {
-  @Test
-  public void bigboitest() {
-    TitleFormatter titleFormatter = new TitleFormatter();
-    String testString = "Life is Good (Official Music Video) (feat. Drake)";
-    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "life is good");
 
-    testString = "Marvins Room";
+  @Test
+  public void simpleCapitalization() {
+    TitleFormatter titleFormatter = new TitleFormatter();
+
+    String testString = "Marvins Room";
     Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "marvins room");
 
-    testString = "Sativa (ft. Swae Lee)";
+    testString = "BLACK PARADE";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "black parade");
+  }
+
+  @Test
+  public void removeOneGroup() {
+    TitleFormatter titleFormatter = new TitleFormatter();
+
+    String testString = "Sativa (ft. Swae Lee)";
     Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "sativa");
 
     testString = "Get You (feat. Kali Uchis)";
@@ -30,32 +37,64 @@ public final class TitleFormatterTest {
     testString = "Promises (feat. Namiko, Miyagi & Miyagi & Namiko)";
     Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "promises");
 
-    testString = "WHATS POPPIN [Remix] (feat. DaBaby, Tory Lanez & Lil Wayne";
+    testString = "Tattoo (Remix)";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "tattoo");
+  }
+
+  @Test
+  public void removeMultipleGroups() {
+    TitleFormatter titleFormatter = new TitleFormatter();
+
+    String testString = "Savage (Remix) (feat. Beyoncé)";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "savage");
+
+    testString = "Life is Good (Official Music Video) (feat. Drake)";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "life is good");
+  }
+
+  @Test
+  public void removeMultipleGroupsSquareAndParen() {
+    TitleFormatter titleFormatter = new TitleFormatter();
+
+    String testString = "WHATS POPPIN [Remix] (feat. DaBaby, Tory Lanez & Lil Wayne)";
     Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "whats poppin");
 
-    testString = "Juice WRLD ft. Marshmello - Come & Go (Official Audio)";
-    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "come & go");
-
-    testString = "Savage (Remix) (feat. Beyoncé)";
-    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "come & go");
-
-    testString = "BLACK PARADE";
-    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "black parade");
+    testString = "Djadja [Remix] (feat. Afro B)";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "djadja");
 
     testString = "Roses (Imanbek Remix) [Latino Gang]";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "roses");
+  }
 
-    testString = "Rod Wave - Girl Of My Dreams (Official Music Video)";
-    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "girl of my dreams");
+  @Test
+  public void removeValidGroups() {
+    TitleFormatter titleFormatter = new TitleFormatter();
 
-    testString = "ily (i love you baby) (feat. Emilee)";
+    String testString = "ily (i love you baby) (feat. Emilee)";
     Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "ily (i love you baby)");
 
     testString = "death bed (coffee for your head)";
     Assert.assertEquals(
         titleFormatter.formatVideoTitle(testString), "death bed (coffee for your head)");
+  }
 
-    testString = "Gucci Mane - Both Sides feat. Lil Baby";
-    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "both sides");
+  @Test
+  public void removeArtistPrefix() {
+    TitleFormatter titleFormatter = new TitleFormatter();
+
+    String testString = "NBA YoungBoy - ALL IN";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "all in");
+  }
+
+  @Test
+  public void removeArtistPrefixAndGroup() {
+    TitleFormatter titleFormatter = new TitleFormatter();
+
+    String testString = "Juice WRLD ft. Marshmello - Come & Go (Official Audio)";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "come & go");
+
+    testString = "Rod Wave - Girl Of My Dreams (Official Music Video)";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "girl of my dreams");
 
     testString = "YG - Swag (Official Music Video)";
     Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "swag");
@@ -63,19 +102,18 @@ public final class TitleFormatterTest {
     testString = "Chase B & Don Toliver - Cafeteria (feat. Gunna) (Official Video)";
     Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "cafeteria");
 
-    testString = "NBA YoungBoy - ALL IN";
-    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "all in");
-
     testString = "King Von - Why He Told (Official Video)";
     Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "why he told");
+  }
 
-    testString = "Tattoo (Remix)";
-    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "tattoo");
+  @Test
+  public void removeUnenclosedKeywords() {
+    TitleFormatter titleFormatter = new TitleFormatter();
 
-    testString = "Djadja [Remix] (feat. Afro B)";
-    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "djadja");
-
-    testString = "La Jeepeta Remix (Lyric Video)";
+    String testString = "La Jeepeta Remix (Lyric Video)";
     Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "la jeepeta");
+
+    testString = "Gucci Mane - Both Sides feat. Lil Baby";
+    Assert.assertEquals(titleFormatter.formatVideoTitle(testString), "both sides");
   }
 }
