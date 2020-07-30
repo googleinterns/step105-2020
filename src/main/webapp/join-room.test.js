2c740f18e1d1b0270a8c7f1c7366463777d3cb3b
Isis Black <isismblack@gmail.com>
2020-07-30 13:40:10 -0400

    Add test cases

--- src/main/webapp/join-room.test.js
+++ src/main/webapp/join-room.test.js
@@ -2,6 +2,30 @@ const script = require('./join-room');
 
 test('get roomId from given url', () => {
   expect(
-    script.parseRoomId("https://localhost:8282/room?roomId=fhaskjdfhkaj")
+    script.parseRoomId("https://localhost:8282/lobby.html?roomId=fhaskjdfhkaj")
   ).toBe("fhaskjdfhkaj")
+})
+
+test('get roomId from given url', () => {
+  expect(
+    script.parseRoomId("localhost:8282/lobby.html?roomId=fhaskjdfhkaj")
+  ).toBe("fhaskjdfhkaj")
+})
+
+test('get roomId from given url', () => {
+  expect(
+    script.parseRoomId("https://localhost:8282/lobby.htmlroomId=fhaskjdfhkaj")
+  ).toBe("")
+})
+
+test('get roomId from given url', () => {
+  expect(
+    script.parseRoomId("https://localhost:8282/lobby.html")
+  ).toBe("")
+})
+
+test('get roomId from given url', () => {
+  expect(
+    script.parseRoomId("https://localhost:8282/lobby.html?roomId=")
+  ).toBe("")
 })
