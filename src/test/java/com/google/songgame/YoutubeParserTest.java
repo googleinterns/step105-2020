b3ab32e60b8617e4a1e364b83b0ff9fec04618bb
dee <hdee@google.com>
2020-07-31 08:31:21 -0400

    add test directory

---
+++ src/test/java/com/google/songgame/YoutubeParserTest.java
@@ -0,0 +1,23 @@
+package com.google.songgame.data;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunsWith(JUnit4.classs)
+public class YoutubeParserTest {
+   
+  private YoutubeParser youtubeParser;
+
+  @Before
+  public void init(){
+    youtubeParser = new YoutubeParser();
+  }
+
+  @Test
+  public void
+    
+}
