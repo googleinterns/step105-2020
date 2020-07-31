f88d71d65ed93a50eccaa486cdc577c964d18330
Salil <salnad02@gmail.com>
2020-07-31 08:43:23 -0400

    Remove unneeded functions which were moved to GuessChecker

--- src/main/java/com/google/songgame/servlets/ChatServlet.java
+++ src/main/java/com/google/songgame/servlets/ChatServlet.java
@@ -30,6 +30,7 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.songgame.data.GuessChecker;
 
 @WebServlet("/chat")
 public final class ChatServlet extends HttpServlet {
@@ -71,24 +72,25 @@ public final class ChatServlet extends HttpServlet {
     return jsonData;
   }
 
-  // TODO: @salilnadkarni modify when points stored in rooms
   private Map<String, String> createPusherChatResponse(Map<String, String> data) {
     Map<String, String> response = new HashMap<String, String>();
 
-    Entity currentGame = getCurrentGame();
-    EmbeddedEntity currentRound = (EmbeddedEntity) currentGame.getProperty("currentRound");
-
-    // get user entity -> call guesschecker to see if prev correct && if correctly guessed
-
     String userId = data.get("userId");
-    String username = getUsername(userId);
+
+    Entity currentGame = getCurrentGame();
+    Entity currentUser = getUser(userId);
+
+    String username = (String) currentUser.getProperty("username");
     String message = data.get("message");
     String messageType = "guess";
 
-    if (checkIfUserPreviouslyGuessedCorrect(userId, currentRound)) {
+
+    if (GuessChecker.hasUserPreviouslyGuessed(currentUser, currentGame)) {
       messageType = "spectator";
-    } else if (isCorrectGuess(message, currentRound)) {
-      markUserGuessedCorrectlyAndAddPoints(userId, currentRound, currentGame);
+    } else if (GuessChecker.isCorrectGuess(message, currentGame)) {
+      currentGame = GuessChecker.markUserGuessedCorrectly(currentUser, currentGame);
+      currentGame = GuessChecker.assignUserPoint(currentUser, currentGame);
+      datastore.put(currentGame);
       messageType = "correct";
       message = "guessed correctly!";
     }
@@ -124,45 +126,12 @@ public final class ChatServlet extends HttpServlet {
     return userId;
   }
 
-  private String getUsername(String userId) {
+  private Entity getUser(String userId) {
     Filter userIdFilter = new FilterPredicate("userId", FilterOperator.EQUAL, userId);
     Query userQuery = new Query("User").setFilter(userIdFilter);
     PreparedQuery result = datastore.prepare(userQuery);
     Entity currentUser = result.asSingleEntity();
-    return (String) currentUser.getProperty("username");
-  }
-
-  private boolean checkIfUserPreviouslyGuessedCorrect(String userId, EmbeddedEntity currentRound) {
-    EmbeddedEntity userGuessStatuses =
-        (EmbeddedEntity) currentRound.getProperty("userGuessStatuses");
-    boolean userGuessStatus = (Boolean) userGuessStatuses.getProperty(userId);
-    return userGuessStatus;
-  }
-
-  private void markUserGuessedCorrectlyAndAddPoints(
-      String userId, EmbeddedEntity currentRound, Entity currentGame) {
-    // Changes the users "guess status" to show they've guessed correctly
-    EmbeddedEntity userGuessStatuses =
-        (EmbeddedEntity) currentRound.getProperty("userGuessStatuses");
-    userGuessStatuses.setProperty(userId, true);
-    
-    // Increases the users points by a fixed amount
-    EmbeddedEntity userPoints = (EmbeddedEntity) currentGame.getProperty("userPoints");
-    long currentUserPoints = (Long) userPoints.getProperty(userId);
-    long updatedUserPoints = currentUserPoints + POINTS_PER_ROUND;
-    userPoints.setProperty(userId, updatedUserPoints);
-    
-    // Update both changes in Datastore
-    currentGame.setProperty("currentRound", currentRound);
-    currentGame.setProperty("userPoints", userPoints);
-    datastore.put(currentGame);
-  }
-
-  private boolean isCorrectGuess(String message, EmbeddedEntity currentRound) {
-    EmbeddedEntity currentVideo = (EmbeddedEntity) currentRound.getProperty("video");
-    String videoTitle = (String) currentVideo.getProperty("title");
-    String guess = message.toLowerCase();
-    return guess.equals(videoTitle);
+    return currentUser;
   }
 
   private void sendResponseToClient(HttpServletResponse response, String message)
