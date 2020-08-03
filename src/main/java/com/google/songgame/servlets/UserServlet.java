fed9ffc902de4e3896aaf9afdbe96f9ed117b095
Isis Black <isismblack@gmail.com>
2020-08-03 10:48:08 -0400

    Create doPut, adds room to user entity

--- src/main/java/com/google/songgame/servlets/UserServlet.java
+++ src/main/java/com/google/songgame/servlets/UserServlet.java
@@ -10,6 +10,9 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,6 +26,7 @@ import java.util.Map;
 import org.apache.hc.core5.http.ParseException;
 import java.lang.reflect.Type;
 import com.google.gson.reflect.TypeToken;
+import javax.servlet.http.Cookie;
 
 @WebServlet("/user")
 public final class UserServlet extends HttpServlet {
@@ -49,9 +53,44 @@ public final class UserServlet extends HttpServlet {
     datastore.put(userEntity);
   }
 
+  @Override
+  public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    Map<String, String> roomProperties = readJSONFromRequest(request);
+
+    // Room query that looks for correct room.
+    Query userQuery =
+        new Query("User").addFilter("userId", FilterOperator.EQUAL, roomProperties.get("userId"));
+    PreparedQuery result = datastore.prepare(userQuery);
+    Entity currentUser = result.asSingleEntity();
+
+    // Add current user to existing datastore list.
+    currentUser.setProperty("roomId", roomProperties.get("roomId"));
+
+    datastore.put(currentUser);
+  }
+
   private Map<String, String> readJSONFromRequest(HttpServletRequest request) throws IOException {
     String requestJSONString = request.getReader().lines().collect(Collectors.joining());
     Map<String, String> jsonData = gson.fromJson(requestJSONString, MESSAGE_TYPE);
+    String userId = getUserId(request);
+    jsonData.put("userId", userId);
     return jsonData;
+  }
+
+  private String getUserId(HttpServletRequest request) throws IOException {
+    Cookie[] cookies = request.getCookies();
+    String userId = "";
+    for (Cookie cookie : cookies) {
+      String name = cookie.getName();
+      if (name.equals("userId")) {
+        userId = cookie.getValue();
+      }
+    }
+
+    if (userId.equals("")) {
+      System.err.println("ERROR: UserID cookie could not be found.");
+    }
+
+    return userId;
   }
 }
