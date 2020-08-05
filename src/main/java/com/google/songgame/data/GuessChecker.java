a486c74024997a0ba038dead649d44dff60a67ca
Salil <salnad02@gmail.com>
2020-08-05 15:14:36 -0400

    Got basic scaled points working

--- src/main/java/com/google/songgame/data/GuessChecker.java
+++ src/main/java/com/google/songgame/data/GuessChecker.java
@@ -3,6 +3,8 @@ package com.google.songgame.data;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.EmbeddedEntity;
 import com.google.cloud.datastore.testing.LocalDatastoreHelper;
+import java.lang.Math;
+import java.util.List;
 
 /**
  * Contains various helper methods revolving around checking a users guess including checking
@@ -61,14 +63,34 @@ public final class GuessChecker {
    *
    * <p>Updates for the given user the point total. Updates in the userPoints map.
    */
-  public static Entity assignUserPoints(Entity user, Entity game) {
+  public static Entity assignUserPoints(Entity user, Entity game, Entity room) {
     String userId = (String) user.getProperty("userId");
     EmbeddedEntity userPoints = (EmbeddedEntity) game.getProperty("userPoints");
+
+    double numUsersGuessedCorrectRatio = getNumUsersGuessedCorrectRatio(game, room);
+
     long currentUserPoints = (Long) userPoints.getProperty(userId);
+    long addedPoints =
+        (long) Math.ceil((double) (POINTS_PER_ROUND) * (1.0 - numUsersGuessedCorrectRatio));
+
     // TODO: @salilnadkarni update to change points given depending on time answered
-    long updatedUserPoints = currentUserPoints + POINTS_PER_ROUND;
+    long updatedUserPoints = currentUserPoints + addedPoints;
     userPoints.setProperty(userId, updatedUserPoints);
     game.setProperty("userPoints", userPoints);
     return game;
+  }
+
+  private static double getNumUsersGuessedCorrectRatio(Entity game, Entity room) {
+    long numUsersGuessed = 0L;
+    EmbeddedEntity currentRound = (EmbeddedEntity) game.getProperty("currentRound");
+    EmbeddedEntity userGuessStatuses =
+        (EmbeddedEntity) currentRound.getProperty("userGuessStatuses");
+    List<String> userIdList = (List<String>) room.getProperty("userIdList");
+    long totalUsers = (long) userIdList.size();
+    for (String userId : userIdList) {
+      boolean userHasGuessed = (Boolean) userGuessStatuses.getProperty(userId);
+      if (userHasGuessed) numUsersGuessed++;
+    }
+    return (double) (numUsersGuessed - 1) / (double) numUsersGuessed;
   }
 }
